34761bb695013a5a799b1ee6b5c16c60197c529a
Paul Wilson <paul.wilson@akqa.com>
2013-09-20 16:57:47 +0100

    more stuff

--- src/config.js
+++ src/config.js
@@ -1,38 +1,39 @@
-/// <reference path="d/node.d.ts" />
-/// <reference path="IConfig.ts" />
-var fs = require("fs");
-var path = require("path");
+define(["require", "exports", "fs", "path"], function(require, exports, __fs__, __path__) {
+    /// <reference path="d/node.d.ts" />
+    /// <reference path="IConfig.ts" />
+    var fs = __fs__;
+    var path = __path__;
 
-var Config = (function () {
-    function Config() {
-        this.dest = "../dest/";
-        this.tempFolder = "../temp/";
-    }
-    return Config;
-})();
-exports.Config = Config;
+    var Config = (function () {
+        function Config() {
+            this.dest = "../dest/";
+            this.tempFolder = "../temp/";
+        }
+        return Config;
+    })();
+    exports.Config = Config;
 
-function loadFromFilePath(pth) {
-    pth = path.normalize(pth);
-    var file = fs.readFileSync(pth, { encoding: "utf8" });
-    return JSON.parse(file);
-}
+    function loadFromFilePath(pth) {
+        pth = path.normalize(pth);
+        var file = fs.readFileSync(pth, { encoding: "utf8" });
+        return JSON.parse(file);
+    }
 
-function load(cfg) {
-    if (typeof (cfg) == "string") {
-        return exports.load(loadFromFilePath(cfg));
+    function load(cfg) {
+        if (typeof (cfg) == "string") {
+            return exports.load(loadFromFilePath(cfg));
+        }
+        var config = new Config();
+        config.urls = cfg.urls;
+        config.widths = cfg.widths;
+        if (cfg.dest) {
+            config.dest = cfg.dest;
+        }
+        if (cfg.tempFolder) {
+            config.tempFolder = cfg.tempFolder;
+        }
+        return config;
     }
-    var config = new Config();
-    config.urls = cfg.urls;
-    config.widths = cfg.widths;
-    if (cfg.dest) {
-        config.dest = cfg.dest;
-    }
-    if (cfg.tempFolder) {
-        config.tempFolder = cfg.tempFolder;
-    }
-    return config;
-}
-exports.load = load;
-
+    exports.load = load;
+});
 //@ sourceMappingURL=config.js.map
